67b6e012135a0d9fb58bbbf4251db62cfb80e3d3
BHANUPRATAP SINGH <bhanurjb21@gmail.com.com>
2025-04-27 23:03:32 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+console.log("Hellow");
+let pencil= 10;
+let pen= 20;
+console.log("total price= ", pencil + pen, "rupees");
+//let output = "total price = " + (pencil + pen) + "rupee";
+//console.log(output);
+let output =`total price = ${pencil + pen} rupees`;
+console.log(output);
+let num =0;
+if(num){
+    console.log("num is not zero");
+}else{
+    console.log("num is zero");
+}
+//alert("Hiii");
+// console.error("it is a error");
+// console.warn("it is a warning");
+// var firstname = prompt("Enter name");
+// console.log(firstname);
+// let pss = prompt("Set password");
+// let newpass =pss.trim();
+// console.log(newpass);
+
+// let msg = "Apna";
+// let upp = msg.toLowerCase();
+// console.log(upp);
+
+// let msg = "Apnafgjoijg";
+// let upp = msg.indexOf("oi");
+// console.log(upp);
+
+let msg = "ApnaCollegeisbest";
+let newmsg = msg.trim();
+let newwwmsg = newmsg.toUpperCase();
+let newly = msg.trim().toUpperCase();
+console.log(newwwmsg);
+console.log(newly);
+console.log(msg.slice(0,4));
---
+++ que.js
@@ -0,0 +1,19 @@
+//ASSIGNMENTS
+// let num = prompt("Enter a number");
+// if(num % 10 === 0){
+//     console.log("Good number");
+// }else{
+//     console.log("bad number");
+// }
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// let output = `${name} is of ${age} year old.`;
+// alert(output);
+
+// let ch = prompt("Enter a word");
+// if ((ch[0] === 'A' || ch[0] === 'a') && ch.length > 5) {
+//     console.log("Golden Word");
+// } else {
+//     console.log("Not a golden word");
+// }
